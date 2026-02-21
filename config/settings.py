ee254b937afc492069254823cd03a43e5132b452
Danzuuu1 <bornea.danilo12@gmail.com>
2026-02-21 13:31:37 +0800

    Change database settings to use PostgreSQL

--- config/settings.py
+++ config/settings.py
@@ -50,10 +50,15 @@ TEMPLATES = [
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'order_db',
+        'USER': 'postgres',
+        'PASSWORD': 'admin',
+        'HOST': 'localhost',
+        'PORT': '5432',
     }
 }
+
 
 LANGUAGE_CODE = 'en-us'
 TIME_ZONE = 'UTC'
